8b8ef23aab2ef98b6086710de4dafe1a31760e7a
Marco Meini <marco@marcomeini.it>
2019-05-13 17:20:02 +0200

    package update

--- test/error.test.ts
+++ test/error.test.ts
@@ -1,31 +1,31 @@
-import { SequelizeError } from '../src/SequelizeError';
-import { expect } from 'chai';
-const Support = require('./support')
+import { SequelizeError } from "../src/SequelizeError";
+import { expect } from "chai";
+const Support = require("./support");
 
-describe('Error', () => {
-  it('Create object with validation errors', (done) => {
-    let support = new Support('mysql')
-    support.model.create({
-      email: 'abcd'
-    }).then(() => {
-      done(new Error('Creation must generate an error'))
-    }).catch((error) => {
-      let sqError = new SequelizeError(error);
-      if (sqError.isValidationError()) {
-        let result = sqError.getResults('There are some errors')
-        let _result = {
-          success: false,
-          message: 'There are some errors',
-          subresults: [
-            { success: false, message: 'table.name cannot be null' },
-            { success: false, message: 'Email is not valid' }
-          ]
+describe("Error", () => {
+  it("Create object with validation errors", done => {
+    let support = new Support("mysql");
+    support.model
+      .create({
+        email: "abcd"
+      })
+      .then(() => {
+        done(new Error("Creation must generate an error"));
+      })
+      .catch(error => {
+        let sqError = new SequelizeError(error);
+        if (sqError.isValidationError()) {
+          let result = sqError.getResults("There are some errors");
+          let _result = {
+            success: false,
+            message: "There are some errors",
+            subresults: [{ success: false, message: "table.name cannot be null" }, { success: false, message: "Email is not valid" }]
+          };
+          expect(result).to.be.deep.equal(_result);
+          done();
+        } else {
+          done(new Error("Validation error not recognised"));
         }
-        expect(result).to.be.deep.equal(_result)
-        done()
-      } else {
-        done(new Error('Validation error not recognised'))
-      }
-    })
-  })
-})
+      });
+  });
+});
